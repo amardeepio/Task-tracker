899fa627741cfb54e2bc5c5bd26775925c18b050
amardeepio <amars123456@gmail.com>
2022-01-12 16:35:32 +0530

    Added JSON Backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useState ,useEffect} from "react"
+import {BrowserRouter as Router , Route, Routes}  from 'react-router-dom'
 import Header from "./components/Header"
 import Task from "./components/Task";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
 
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
@@ -19,44 +22,65 @@ const StyledApp = styled.div`
 const App =() =>{
   const [showAddTask, setShowAddTask] = useState(false)
   const [theme, setTheme] = useState("light");
-  const[tasks, setTasks] = useState([
+  const[tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+
+      const taskFromServer = await fetchTasks()
+      setTasks(taskFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+// fetch Tasks data
+
+const fetchTasks = async () => {
+  const res = await fetch('http://localhost:5000/tasks')
+  const data = await res.json()
+
+  return data
+
+}
 
 
-    {
-        id: 1,
-        text: 'Doctors appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
+// fetch Task data
 
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
 
-    },
+  return data
 
-    {
-
-        id: 3,
-        text: 'food shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-
-])
+}
 
 //Add task
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  })
 
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 1000) +1
-  const newTask = {id, ...task}
-  setTasks([...tasks,newTask])
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+  // const id = Math.floor(Math.random() * 10000) + 1
+  // const newTask = { id, ...task }
+  // setTasks([...tasks, newTask])
 }
 
 //delete Task
-const DeleteTask = (id) => {
+const DeleteTask = async (id) => {
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE',
+  })
+
 
   setTasks(tasks.filter((task) => task.id !== id))
 
@@ -64,9 +88,23 @@ const DeleteTask = (id) => {
 
 //Toggle Reminder
 
-const toggleReminder = (id) => {
+const toggleReminder = async(id) => {
 
-  setTasks(tasks.map((task) => task.id === id ? {...task,reminder: !task.reminder} : task))
+  const taskToToggle = await fetchTask(id)
+  const updTask  = {...taskToToggle,reminder: !taskToToggle.reminder}
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => task.id === id ? {...task,reminder: data.reminder} : task))
 }
 
 
@@ -80,21 +118,46 @@ const themeToggler = () => {
   return (
 
     
-
+    <Router>
     <div className="container">
         <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <Header onAdd={() =>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+
+      <Routes>
+        <Route
+        path='/'
+        element= {
+
+        <>
+      
+
       {showAddTask &&<AddTask onAdd={addTask}/>}
+      
       {tasks.length >0 ? <Tasks tasks={tasks} onDelete={DeleteTask} onToggle ={toggleReminder}/> : 'No tasks to show'}
+
+      </>
+        }
+        />
+
+        <Route path='/about' element={<About/>} />
+        </Routes>
+        <Footer/>
       <GlobalStyles />
       <StyledApp>
       
         <button onClick={() => themeToggler()} type="button" class="glow-on-hover"  >Dark Theme</button> {' '}
       </StyledApp>
       </ThemeProvider>
+
+      
+      
+    
+  
+    
     </div>
 
-
+ 
+    </Router>
 
   );
 }
